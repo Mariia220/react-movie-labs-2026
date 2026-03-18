6b5f63350a01bfe0e906b6ff7187fefd9c6f94ce
Mariia Popovych <20107971@setu.ie>
2026-03-18 19:00:22 +0000

    We have updated the actor's page and added navigation between pages

--- movies/src/pages/actorDetailsPage.jsx
+++ movies/src/pages/actorDetailsPage.jsx
@@ -9,6 +9,7 @@ import Grid from "@mui/material/Grid";
 import { Link } from "react-router";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
 const ActorDetailsPage = () => {
   const { id } = useParams(); 
@@ -27,43 +28,47 @@ const ActorDetailsPage = () => {
   if (creditsPending) return <Spinner />;
 
   return (
-    <div style={{ padding: "20px" }}>
+    <Box sx={{ p: { xs: 2, md: 4 }, flexGrow: 1 }}>
       {actor ? (
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={4}>
-            <Paper elevation={3} sx={{ p: 2 }}>
+        <Grid container spacing={4}>
+          <Grid size ={{ xs: 12, sm: 5, md: 4 }}>
+            <Paper elevation={12} sx={{ p: 2, borderRadius: 2 }}>
               <img
                 src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
                 alt={actor.name}
-                style={{ width: "100%", borderRadius: "8px" }}
+                style={{ width: "100%", borderRadius: "8px", transition: '0.3s' }}
+                onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.02)'}
+                onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1.0)'}
               />
             </Paper>
           </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography variant="h3" component="h1" gutterBottom>
+          <Grid size ={{ xs: 12, sm: 7, md: 8 }}>
+            <Typography variant="h3" component="h1" gutterBottom sx={{ fontWeight: 'bold' }}>
               {actor.name}
             </Typography>
 
-            <Typography variant="h5" gutterBottom>
+            <Typography variant="h5" color="text.secondary" gutterBottom sx={{ mt: 3, fontStyle: 'italic' }}>
               Biography
             </Typography>
 
-            <Typography variant="body1" gutterBottom>
+            <Typography variant="body1" color="text.primary" sx={{ mb: 4, lineHeight: 1.8 }}>
               {actor.biography || "No biography available for this actor."}
             </Typography>
 
-            <Paper elevation={2} sx={{ p: 2, backgroundColor: "#f5f5f5" }}>
-              <Typography variant="h6">Personal Info</Typography>
-              <Typography>Known for: {actor.known_for_department}</Typography>
-              <Typography>Birthday: {actor.birthday}</Typography>
-              <Typography>Place of Birth: {actor.place_of_birth}</Typography>
+            <Paper elevation={1} sx={{ p: 3, mb: 4, backgroundColor: "#f5f5f5", borderRadius: 2 }}>
+              <Typography variant="h6" sx={{ color: 'primary.main', fontWeight: 'medium' }}>Personal Info</Typography>
+              <Box sx={{ mt: 1 }}>
+              <Typography><strong>Known for:</strong> {actor.known_for_department}</Typography>
+                <Typography><strong>Birthday:</strong> {actor.birthday}</Typography>
+                <Typography><strong>Place of Birth:</strong> {actor.place_of_birth}</Typography>
+              </Box>
             </Paper>
 
-            <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
-              Known For (Movies)
+            <Typography variant="h5" component="h3" gutterBottom sx={{ mt: 4, color: 'secondary.main', fontWeight: 'bold' }}>
+              Films with {actor.name}
             </Typography>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Stack direction="row" flexWrap="wrap" gap={1}>
+            <Paper elevation={1} sx={{ p: 2, borderRadius: 2}}>
+              <Stack direction="row" flexWrap="wrap" gap={1.5}>
                 {credits.cast && credits.cast.slice(0, 12).map((m) => (
                   <Link key={m.id} to={`/movies/${m.id}`} style={{ textDecoration: 'none' }}>
                     <Chip 
@@ -71,7 +76,15 @@ const ActorDetailsPage = () => {
                       variant="outlined" 
                       clickable 
                       color="primary"
-                      sx={{ m: 0.5 }}
+                     sx={{
+                        m: 0.5,
+                        borderRadius: '16px',
+                        '&:hover': {
+                          backgroundColor: 'secondary.light',
+                          color: 'secondary.contrastText',
+                          transition: '0.3s'
+                        }
+                      }}
                     />
                   </Link>
                 ))}
@@ -82,7 +95,7 @@ const ActorDetailsPage = () => {
       ) : (
         <Typography variant="h4">Actor details not found</Typography>
       )}
-    </div>
+      </Box>
   );
 };
 
